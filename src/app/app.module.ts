3c9b5a60992f56667238463cf5a2436a1607521b
Dener da Silva Cardoso <denercardoso.2013@gmail.com>
2022-06-08 15:06:33 -0300

    Implementado calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './Calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule,
